ddd640e17570ada32221a7269ef66b8557008bc2
Nathaniel Nunes <manutdkid77@gmail.com>
2019-04-17 17:02:43 +0530

    Disable input for scrNames, and set ScrollToAsync for scrNames with animation false. This allows for instant scroll

--- Cally/Cally/Views/MainPage.xaml.cs
+++ Cally/Cally/Views/MainPage.xaml.cs
@@ -15,119 +15,65 @@ namespace Cally.Views
         public MainPage()
         {
             InitializeComponent();
-            scrNames.Scrolled += ScrNames_Scrolled;
             scrCal.Scrolled += ScrCal_Scrolled;
-            BindEmployeeNames();
-            BindCalendar();
+            BindCal();
+            BindNames();
+        }
+
+        private void BindCal()
+        {
+            for (var i = 0; i < 70; i++)
+            {
+                var stk = new StackLayout();
+                stk.Orientation = StackOrientation.Horizontal;
+                for (var j = 0; j < 10; j++)
+                {
+                    var lbl = new Label()
+                    {
+                        Text = $"Row{i}, Day {j}",
+                        TextColor = Color.Black,
+                        BackgroundColor = Color.Yellow
+                    };
+                    stk.Children.Add(lbl);
+                }
+                stkCal.Children.Add(stk);
+            }
+        }
+
+        private void BindNames()
+        {
+            for (var i = 0; i < 70; i++)
+            {
+                var lbl = new Label()
+                {
+                    Text = $"Employee {i}",
+                    TextColor = Color.Black,
+                    BackgroundColor = Color.Yellow
+                };
+                stkNames.Children.Add(lbl);
+            }
         }
 
         private async void ScrNames_Scrolled(object sender, ScrolledEventArgs e)
         {
-            try
-            {
-                if (bscrCalScrolled || e.ScrollY <= -1)
-                    return;
+            if (bscrCalScrolled || e.ScrollY <= -1)
+                return;
 
-                bscrNamesScrolled = true;
+            bscrNamesScrolled = true;
 
-                await scrCal.ScrollToAsync(0, e.ScrollY, true);
-            }
-            finally
-            {
-                bscrNamesScrolled = false;
-            }
+            await scrCal.ScrollToAsync(0, e.ScrollY, true);
+            bscrNamesScrolled = false;
         }
 
         private async void ScrCal_Scrolled(object sender, ScrolledEventArgs e)
         {
-            try
-            {
-                if (bscrNamesScrolled || e.ScrollY <= -1)
-                    return;
-                bscrCalScrolled = true;
+            if (bscrNamesScrolled || e.ScrollY <= -1)
+                return;
+            bscrCalScrolled = true;
 
-                await scrNames.ScrollToAsync(0, e.ScrollY, true);
-            }
-            finally
-            {
-                bscrCalScrolled = false;
-            }
-        }
-
-        private void BindEmployeeNames()
-        {
-            try
-            {
-                for (var i = 1; i <= 40; i++)
-                {
-                    stkEmpNames.Children.Add(new Label()
-                    {
-                        Text = $"Employee {i}",
-                        TextColor = Color.Black,
-                        LineBreakMode = LineBreakMode.TailTruncation,
-                        WidthRequest = 50,
-                        HeightRequest = 16
-                    });
-                    stkEmpNames.Children.Add(new BoxView()
-                    {
-                        Color = Color.Black,
-                        HeightRequest = 0.5,
-                        HorizontalOptions = LayoutOptions.FillAndExpand
-                    });
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        private void BindCalendar()
-        {
-            try
-            {
-                for (var i = 1; i <= 280; i++)
-                {
-                    var lbl = new Label()
-                    {
-                        Text = $"Item{i}",
-                        TextColor = Color.Black,
-                        HeightRequest = 16
-                    };
-                    flxCal.Children.Add(lbl);
-                    FlexLayout.SetGrow(lbl, 1);
-                    FlexLayout.SetShrink(lbl, 0);
-                    FlexLayout.SetBasis(lbl, new FlexBasis(50f));
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        private void btnHorScrl_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        private void btnVerScrl_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                //scrlVwParent.InputTransparent = false;
-            }
-            catch (Exception ex)
-            {
-
-            }
+            //animation false allows for instant scroll instead of waiting for animation to complete
+            await scrNames.ScrollToAsync(0, e.ScrollY, false);
+            bscrCalScrolled = false;
         }
     }
 }
