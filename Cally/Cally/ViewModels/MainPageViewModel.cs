80a5053c404973b6068857b53bcf4703eb756ab8
Nathaniel Nunes <manutdkid77@gmail.com>
2019-04-21 16:55:15 +0530

    Randomly select from 5 Schedules

--- Cally/Cally/ViewModels/MainPageViewModel.cs
+++ Cally/Cally/ViewModels/MainPageViewModel.cs
@@ -19,10 +19,12 @@ namespace Cally.ViewModels
             set { SetProperty(ref _obsEmployee, value); }
         }
 
+        Random rnd = new Random();
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
-            Title = "Main Page";
+            Title = "Work Schedule";
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
@@ -36,22 +38,13 @@ namespace Cally.ViewModels
             try
             {
                 var lstEmployee = new List<Employee>();
-                for(var i = 1; i <= 40; i++)
+                for(var i = 1; i <= 100; i++)
                 {
                     lstEmployee.Add(new Employee()
                     {
                         ID = i,
                         Name = $"Employee {i}",
-                        Schedule = new Schedule()
-                        {
-                            Monday = "Work",
-                            Tuesday = "Work",
-                            Wednesday = "Work",
-                            Thursday = "HalfDay",
-                            Friday = "Holiday",
-                            Saturday = "Holiday",
-                            Sunday = "Easter"
-                        }
+                        Schedule = GenerateRandomSchedule()
                     });
                 }
                 obsEmployee = new ObservableCollection<Employee>(lstEmployee);
@@ -60,6 +53,65 @@ namespace Cally.ViewModels
             {
 
             }
+        }
+        
+        private Schedule GenerateRandomSchedule()
+        {
+            var lstRandomSchedule = new List<Schedule>()
+            {
+                new Schedule()
+                {
+                    Monday = "Unavailable",
+                    Tuesday = "Manager",
+                    Wednesday = "Manager",
+                    Thursday = "Manager",
+                    Friday = "Manager",
+                    Saturday = "Holiday",
+                    Sunday = "Holiday"
+                },
+                new Schedule()
+                {
+                    Monday = "Manager",
+                    Tuesday = "Front Desk",
+                    Wednesday = "Holiday",
+                    Thursday = "Supervisor",
+                    Friday = "Floor",
+                    Saturday = "Holiday",
+                    Sunday = "Holiday"
+                },
+                new Schedule()
+                {
+                    Monday = "Floor",
+                    Tuesday = "Floor",
+                    Wednesday = "Supervisor",
+                    Thursday = "Supervisor",
+                    Friday = "Holiday",
+                    Saturday = "Half Day",
+                    Sunday = "Holiday"
+                },
+                new Schedule()
+                {
+                    Monday = "Team Lead",
+                    Tuesday = "Manager",
+                    Wednesday = "Team Lead",
+                    Thursday = "Inventory",
+                    Friday = "Unavailable",
+                    Saturday = "Holiday",
+                    Sunday = "Holiday"
+                },
+                new Schedule()
+                {
+                    Monday = "Floor",
+                    Tuesday = "Floor",
+                    Wednesday = "Inventory",
+                    Thursday = "Manager",
+                    Friday = "Holiday",
+                    Saturday = "Holiday",
+                    Sunday = "Manager"
+                },
+            };
+            var iRandomNo = rnd.Next(0, 4);
+            return lstRandomSchedule[iRandomNo];
         }
     }
 }
